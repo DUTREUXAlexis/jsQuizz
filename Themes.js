b24542a2233ed8fea738466f268318d51464dd81
DUTREUXA <dutreuxa@gmail.com>
2023-04-24 09:17:59 +0200

    suite

--- Themes.js
+++ Themes.js
@@ -1,23 +1,33 @@
-
 const getThemes = () => {
-    return fetch( '/api/theme')
+    return fetch( 'http://127.0.0.1:8000/api/theme')
         .then(response=>
         {
-            console.log(response)
-            //tester si la reponse est OK
             if(response.ok) {
                 return response.json()
             }
             throw new Error("Une erreur est survenue, vérifier votre endPoint.")
         })
-        .catch(response =>
-        {
-            console.log('dsqdsqdq')
-        })
 }
 //rendre disponible cette fonction dans d'autres modules :
 getThemes()
-  .then((resultat)=>console.log(resultat))
-  .catch((resultat) => console.log(resultat))
-//export {getThemes}
-//
+  .then((resultat)=>displayThemes(resultat))
+  .catch(error => console.log(error))
+
+
+export {getThemes}
+
+const displayThemes = (Themes) => {
+    const tr = document.createElement('tr')
+    const tbody = document.querySelector('tbody')
+    Themes.forEach( theme => {
+        const listeDeroulante = document.getElementById('Themes')
+        const a = document.createElement('a')
+        a.textContent = theme.libelle
+        listeDeroulante.appendChild(a)
+
+        tbody.appendChild(tr)
+    })
+
+}
+
+displayThemes()
