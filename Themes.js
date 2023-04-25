251abf4355fcdbdabeab079371a4f619c4dd2ddc
DUTREUXAlexis <dutreuxa@gmail.com>
2023-04-26 00:34:33 +0200

    Liste déroulante sans données

--- Themes.js
+++ Themes.js
@@ -1,33 +1,33 @@
 const getThemes = () => {
-    return fetch( 'http://127.0.0.1:8000/api/theme')
-        .then(response=>
-        {
-            if(response.ok) {
-                return response.json()
+    return fetch('http://127.0.0.1:8000/api/themes', { mode: 'no-cors' })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
             }
-            throw new Error("Une erreur est survenue, vérifier votre endPoint.")
-        })
-}
-//rendre disponible cette fonction dans d'autres modules :
+            throw new Error("Une erreur est survenue, vérifier votre endPoint.");
+        });
+};
+
+const displayThemes = (themes) => {
+    const tbody = document.querySelector('tbody');
+    const select = document.getElementById('Themes');
+
+    themes.forEach(theme => {
+        // Ajouter une ligne au corps de tableau
+        const tr = document.createElement('tr');
+        const tdTitre = document.createElement('td');
+        tdTitre.textContent = theme.Titre;
+        console.log(tdTitre.textContent)
+        tr.appendChild(tdTitre);
+        tbody.appendChild(tr);
+
+        // Ajouter une option à la liste déroulante
+        const option = document.createElement('option');
+        option.textContent = theme.Titre;
+        select.appendChild(option);
+    });
+};
+
 getThemes()
-  .then((resultat)=>displayThemes(resultat))
-  .catch(error => console.log(error))
-
-
-export {getThemes}
-
-const displayThemes = (Themes) => {
-    const tr = document.createElement('tr')
-    const tbody = document.querySelector('tbody')
-    Themes.forEach( theme => {
-        const listeDeroulante = document.getElementById('Themes')
-        const a = document.createElement('a')
-        a.textContent = theme.libelle
-        listeDeroulante.appendChild(a)
-
-        tbody.appendChild(tr)
-    })
-
-}
-
-displayThemes()
+    .then(resultat => displayThemes(resultat))
+    .catch(error => console.log(error));
